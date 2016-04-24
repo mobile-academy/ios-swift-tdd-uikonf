7b63650c0ee2ec982d902d3a22853c8486dad709
Paweł Dudek <pawel@dudek.mobi>
2016-04-24 15:48:06 +0200

    Initial Commit

---
+++ UIKonf/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  UIKonf
+//
+//  Created by Paweł Dudek on 24/04/16.
+//  Copyright © 2016 Mobile Academy. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
